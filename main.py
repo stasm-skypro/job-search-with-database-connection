e66044a9fa977044ef9bee4a87a0fa1e36cd9689
Станислав Маяцкий <stasm226@gmail.com>
2024-12-03 18:24:49 +0500

    refactored get_vacancies_with_keyword function and implemented main function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.config import config
+from src.db_manager import DBManager
 from src.headhunter_api import HeadHunterAPI
 from src.schema_manager import SchemaManager
 
 
-def main() -> None:
+def main(*args) -> None:
     """
     Код для получения данных о вакансиях из API HeadHunter, создания базы данных и взаимодействия с пользователем
     """
@@ -11,45 +12,57 @@ def main() -> None:
     # Аргумент keyword определяет слово, по которому будет осуществлён поиск.
     # Аргумент pages определяет количество страниц, в которых будет осуществлён поиск.
     # Аргумент per_page определяет количество вакансий на странице.
+    print("================ Выполним запрос на сайте hh.ru ====================")
     print("Введём входные данные для поиска вакансий")
+    if args:
 
-    # keyword = "python"
-    user_input = input("Введите ключевое слово (по умолчанию 'Python'): ").lower()
-    keyword = user_input if user_input else "python"
+        keyword = "python"
+        pages = 1
+        per_page = 10
 
-    # pages = 10
-    user_input = input("Введите количество страниц с вакансиями, которые вы хотите получить от 1 до 10 (по умолчанию 1): ").lower()
-    pages = user_input if user_input else 1
+    else:
 
-    # per_page = 100
-    user_input = input("Введите количество вакансий на странице от 10 до 100 (по умолчанию 10): ").lower()
-    per_page = user_input if user_input else 10
+        # keyword = "python"
+        user_input = input("Введите ключевое слово (по умолчанию 'Python'): ").lower()
+        keyword = user_input if user_input else "python"
+
+        # pages = 10
+        user_input = input("Введите количество страниц с вакансиями, которые вы хотите получить от 1 до 10 (по умолчанию 1): ").lower()
+        pages = user_input if user_input else 1
+
+        # per_page = 100
+        user_input = input("Введите количество вакансий на странице от 10 до 100 (по умолчанию 10): ").lower()
+        per_page = user_input if user_input else 10
 
     print(f"Вы ввели: ключевое слово - {keyword}, запрошено страниц - {pages}, вакансий на странице - {per_page}")
     print("Получим сырые данные из API")
 
-    # --Создадим экземпляра класса для работы с API headhunter
+    # --Создадим экземпляр класса для работы с API headhunter
     base_url: str = "https://api.hh.ru/vacancies"
     hh_api = HeadHunterAPI(url=base_url, pages=pages, per_page=per_page)
     hh_vacancies = hh_api.load_vacancies(keyword=keyword)
     print(f"Получено {len(hh_vacancies)} вакансий")
 
     # --Запишем результат запроса в json-файл
+    print("Запишем результат запроса в json-файл 'data/data.json'")
     from src.file_utils import JsonWorker
 
     json_worker = JsonWorker("data/data.json")
     json_worker.write_file(hh_vacancies)
 
     # -------------------- СОЗДАНИЕ БАЗЫ ДАННЫХ -------------------------------
+    print("========= Создадим базу данных 'headhunter' PostgreSQL  ============")
     init_connection_parameters = (
         config()
     )  # {"host": "localhost", "user": "postgres", "password": "****", "port": 5433}
 
     # --Создадим базу данных
+    print("Создадим базу данных")
     sm = SchemaManager(connection_parameters=init_connection_parameters)
     sm.create_database(data_base_name="headhunter")
 
     # --Создадим таблицу companies для хранения информации о компаниях
+    print("Создадим таблицу companies для хранения информации о компаниях")
     query_to_create_companies_table = """
                 CREATE TABLE %s (
                     company_id VARCHAR(10) PRIMARY KEY,
@@ -63,7 +76,8 @@ def main() -> None:
                 """
     sm.create_table(data_base_name="headhunter", table_name="companies", query=query_to_create_companies_table)
 
-    # --Создадим таблицу companies для хранения информации о вакансиях
+    # --Создадим таблицу vacancies для хранения информации о вакансиях
+    print("Создадим таблицу vacancies для хранения информации о вакансиях")
     query_to_create_vacancies_table = """
                 CREATE TABLE %s (
                     vacancy_id VARCHAR(10) PRIMARY KEY,
@@ -84,10 +98,71 @@ def main() -> None:
     data = json_worker.read_file()
 
     # --Добавим данные из объекта data в таблицы (заполним таблицы)
+    print("Заполним таблицы")
     sm.insert_data(data_base_name="headhunter", vacancies_data=data)
+    print("Таблицы готовы")
 
+    # ------------ ВЫПОЛНЕНИЕ ЗАПРОСА ПОЛЬЗОВАТЕЛЯ К БАЗЕ ДАННЫХ --------------
+    excepted_queries = {
+        "1":"Получить список всех компаний и количество вакансий у каждой компании",
+        "2":"Получить список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на "
+            "вакансию",
+        "3":"Получить среднюю зарплату по вакансиям",
+        "4":"Получить список всех вакансий, у которых зарплата выше средней по всем вакансиям",
+        "5":"Получить список всех вакансий, в названии которых содержатся ключевые слова",
+    }
+    # --Выполним запрос к полученной базе данных.
+    print("Выполним запрос к полученной базе данных")
+    print("Список доступных запросов:")
+    for key, value in excepted_queries.items():
+        print("%s - %s" % (key, value))
 
+    user_input = input("Введите номер желаемого запроса (по умолчанию - 1): ").lower()
+    query_number = user_input if user_input else "1"
+    print("Вы выбрали запрос - %s" % excepted_queries[query_number])
+    print("Результат запроса:", "\n")
+    dbm = DBManager(connection_parameters=init_connection_parameters)
+
+    match query_number:
+        case "1":
+            # Получим список всех компаний и количество вакансий у каждой компании
+            result = dbm.get_companies_and_vacancies_count(data_base_name="headhunter")
+            print(*list("Компания %s: %d вакансий" % (item[0], item[1]) for item in result), sep="\n")
+            print()
+
+        case "2":
+            # Получим список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию
+            result = dbm.get_all_vacancies(data_base_name="headhunter")
+            for item in result:
+                print("Требуется %s в компанию '%s', зарплата %s %s, ссылка на вакансию: %s " % tuple(x for x in item))
+            print()
+
+        case "3":
+            # Получим среднюю зарплату по вакансиям
+            result = dbm.get_avg_salary(data_base_name="headhunter")
+            print(f"Средняя зарплата по вакансиям - {round(result[0][0])} {result[0][1]}")
+
+        case "4":
+            # Получим список всех вакансий, у которых зарплата выше средней по всем вакансиям
+            result = dbm.get_vacancies_with_higher_salary(data_base_name="headhunter")
+            for item in result:
+                print("%s зарплата - %s %s" % item)
+            print()
+
+        case "5":
+            # Получим список всех вакансий, в названии которых содержатся переданные в метод слова
+            user_input = input("Введите ключевые слова для поиска через пробел (ключевое слово по умолчанию "
+                               "- 'разработчик программист')").lower()
+            kw = user_input if user_input else "разработчик программист"
+            result = dbm.get_vacancies_with_keyword(data_base_name="headhunter", keywords=kw)
+            if not result:
+                print("Вакансий с ключевыми словами %s нет в базе" % kw)
+            else:
+                for item in result:
+                    print("%s зарплата - %s %s" % item)
+            print()
 
 
 if __name__ == "__main__":
-    main()
+    # Если передать любой аргумент, то программа пропустит пользовательский запрос и примет значения по умолчанию
+    main(1)
--- src/db_manager.py
+++ src/db_manager.py
@@ -15,7 +15,7 @@ class DBManager:
         Получает список всех компаний и количество вакансий у каждой компании.
         """
         conn = psycopg2.connect(dbname=data_base_name, **self.__params)
-        result: list[tuple] = []
+        res: list[tuple] = []
         with conn.cursor() as cur:
             cur.execute(
                 """
@@ -25,11 +25,11 @@ class DBManager:
                 ORDER BY company_name
                 """
             )
-            result = cur.fetchall()
+            res = cur.fetchall()
 
         conn.close()
 
-        return result
+        return res
 
     def get_all_vacancies(self, data_base_name: str) -> list[tuple]:
         """
@@ -37,7 +37,7 @@ class DBManager:
         вакансию.
         """
         conn = psycopg2.connect(dbname=data_base_name, **self.__params)
-        result: list[tuple] = []
+        res: list[tuple] = []
         with conn.cursor() as cur:
             cur.execute(
                 """
@@ -47,18 +47,40 @@ class DBManager:
                 ORDER BY vacancy_name
                 """
             )
-            result = cur.fetchall()
+            res = cur.fetchall()
 
         conn.close()
 
-        return result
+        return res
 
     def get_avg_salary(self, data_base_name: str) -> list[tuple]:
+        """
+        Получает среднюю зарплату по вакансиям
+        """
+        conn = psycopg2.connect(dbname=data_base_name, **self.__params)
+        res: list[tuple]
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT AVG(salary), salary_currency
+                FROM vacancies
+                WHERE salary <> 0
+                GROUP BY salary_currency
+                """
+            )
+            res = cur.fetchall()
+
+        conn.close()
+
+        return res
+
+
+    def get_vacancies_with_higher_salary(self, data_base_name: str) -> list[tuple]:
         """
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         """
         conn = psycopg2.connect(dbname=data_base_name, **self.__params)
-        result: list[tuple] = []
+        res: list[tuple] = []
         with conn.cursor() as cur:
             cur.execute(
                 """
@@ -67,32 +89,40 @@ class DBManager:
                 ORDER BY salary DESC
                 """
             )
-            result = cur.fetchall()
+            res = cur.fetchall()
 
         conn.close()
 
-        return result
+        return res
 
-    def get_vacancies_with_keyword(self, data_base_name: str, keyword: str) -> list[tuple]:
+    def get_vacancies_with_keyword(self, data_base_name: str, keywords: str) -> list[tuple]:
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова.
         """
         conn = psycopg2.connect(dbname=data_base_name, **self.__params)
-        result: list[tuple] = []
+        res: list[tuple] = []
         with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT vacancy_name, salary, salary_currency FROM vacancies
-                WHERE %s IN (vacancy_name)
-                ORDER BY vacancy_name
-                """,
-                (keyword,),
-            )
-            result = cur.fetchall()
+            keyword_list = keywords.split()
+
+            try:
+                # Формируем условие WHERE с использованием ILIKE (выполняет поиск независимо от регистра)
+                where_clause = " OR ".join([f"vacancy_name ILIKE %s" for _ in keyword_list])
+
+                # Полный SQL-запрос
+                query = f"SELECT vacancy_name, salary, salary_currency FROM vacancies WHERE {where_clause} ORDER BY vacancy_name"
+
+                # Выполнение параметризованного запроса
+                cur.execute(query, [f"%{keyword}%" for keyword in keyword_list])
+
+            except KeyError:
+                res = []
+
+            else:
+                res = cur.fetchall()
 
         conn.close()
 
-        return result
+        return res
 
 
 if __name__ == "__main__":
@@ -100,19 +130,38 @@ if __name__ == "__main__":
     dbm = DBManager(connection_parameters=init_connection_parameters)
 
     # Получим список всех компаний и количество вакансий у каждой компании
+    print("Получим список всех компаний и количество вакансий у каждой компании")
     result = dbm.get_companies_and_vacancies_count(data_base_name="headhunter")
     print(*list("Компания %s: %d вакансий" % (item[0], item[1]) for item in result), sep="\n")
+    print()
 
     # Получим список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию
+    print("Получим список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию")
     result = dbm.get_all_vacancies(data_base_name="headhunter")
     for item in result:
         print("Требуется %s в компанию '%s', зарплата %s %s, ссылка на вакансию: %s " % tuple(x for x in item))
-    # Получим список всех вакансий, у которых зарплата выше средней по всем вакансиям
+    print()
+
+    # Получим среднюю зарплату по вакансиям
+    print("Получим среднюю зарплату по вакансиям")
     result = dbm.get_avg_salary(data_base_name="headhunter")
+    print(f"Средняя зарплата по вакансиям - {round(result[0][0], 2)} {result[0][1]}")
+    print()
+
+    # Получим список всех вакансий, у которых зарплата выше средней по всем вакансиям
+    print("Получим список всех вакансий, у которых зарплата выше средней по всем вакансиям")
+    result = dbm.get_vacancies_with_higher_salary(data_base_name="headhunter")
     for item in result:
         print("%s зарплата - %s %s" % item)
+    print()
 
     # Получим список всех вакансий, в названии которых содержатся переданные в метод слова
-    result = dbm.get_vacancies_with_keyword(data_base_name="headhunter", keyword="аналитик")
-    for item in result:
-        print("%s зарплата - %s %s" % item)
+    kw = "разработчик программист"
+    print("Получим список всех вакансий, в названии которых содержатся переданные в метод слова %s" % kw)
+    result = dbm.get_vacancies_with_keyword(data_base_name="headhunter", keywords=kw)
+    if not result:
+        print("Вакансий с ключевыми словами %s нет в базе" % kw)
+    else:
+        for item in result:
+            print("%s зарплата - %s %s" % item)
+    print()
